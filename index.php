0bf106bcf46f467f63c7dbe7983df36f7832e312
acc-cloudaccess <acc.cloudaccess@gmail.com>
2017-05-31 12:45:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ else
     <section class="congratulations">
         <h1>Congratulations!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP version <?= phpversion() ?></p>
+        <p>PHP version <?= phpversion() ?></p>
     </section>
 
     <section class="instructions">
